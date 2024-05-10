2829faafde7a55e85f0631eddc436790f88f8215
Samuel Monson <smonson@irbash.net>
2024-05-10 10:15:25 -0700

    Support specifing strings to collect

--- bench.py
+++ bench.py
@@ -11,31 +11,41 @@ from pprint import pprint
 TRIALS  = 4
 LOG_CNT = 10
 
-def exec(*args):
-    print(' '.join(args), file=sys.stderr)
-    proc = subprocess.run(args, capture_output=True)
-    output = proc.stdout.decode()
-    runtime = re.findall(r"^Runtime\s*:\s*(\d*\.?\d*)$", output, re.M)
-    try:
-        return float(runtime[0])
-    except IndexError:
-        results = {
-            'args'   : proc.args,
-            'stdout' : output.split("\n")[-LOG_CNT:],
-            'stderr' : proc.stderr.decode().split("\n")[-LOG_CNT:]
-        }
-        print("Failed: ", end='', file=sys.stderr)
-        pprint(results, stream=sys.stderr)
-        return float("inf")
+class Benchmark(object):
+    def __init__(self, records: dict[str, dict], args: tuple[str]):
+        self.records = records
+        self.args = args
+        self.order_by = next(iter(records))
 
-def benchmark(input: csv.reader, output: csv.writer, *args):
-    for row in input:
-        new_args = [ a.format(**row) for a in args ]
-        time = float("inf")
-        for i in range(TRIALS):
-            time = min(time, exec(*new_args))
-        row['time'] = time
-        writer.writerow(row)
+    def exec(self, args):
+        print(' '.join(args), file=sys.stderr)
+        proc = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = proc.stdout.decode()
+        results = {}
+        for key, val in self.records.items():
+            m = re.search(val, output)
+            if m:
+                results[key] = float(m.group(key))
+            else:
+                results = {
+                    'args'   : proc.args,
+                    'output' : output.split("\n")[-LOG_CNT:],
+                }
+                print("Failed: ", end='', file=sys.stderr)
+                pprint(results, stream=sys.stderr)
+                return None
+        return results
+
+    def run(self, input: csv.reader, output: csv.writer):
+        for row in input:
+            new_args = [ a.format(**row) for a in self.args ]
+            best = { rec : float("inf") for rec in self.records }
+            for i in range(TRIALS):
+                results = self.exec(new_args)
+                if results[self.order_by] < best[self.order_by]:
+                    best = results
+            row.update(best)
+            writer.writerow(row)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -43,10 +53,14 @@ if __name__ == '__main__':
                         default=sys.stdin, type=argparse.FileType('r'))
     parser.add_argument("-o", "--out", dest='outfile', help="Output file",
                         default=sys.stdout, type=argparse.FileType('w'))
+    parser.add_argument("-r", "--record", dest='records',
+                        help="Stores a value matching the second argument into a column matching the first",
+                        action='append', nargs=2, required=True)
     # TODO: Make -- COMMAND show up in help
     # parser.add_argument("--", dest='command', help="Command",
     #                     type=str, action='extend', required=True)
 
+    # Parse command string
     try:
         idx = sys.argv.index('--')
     except ValueError:
@@ -56,9 +70,26 @@ if __name__ == '__main__':
     sys.argv = sys.argv[:idx]
 
     args = parser.parse_args()
+
+    # Define output format
+    outputs = {}
+    for record in args.records:
+        if record[0] in outputs:
+            print("Duplicate key", record[0], file=sys.stderr)
+            sys.exit(2)
+        pattern = re.compile(record[1], re.M)
+        if record[0] not in pattern.groupindex:
+            print("Pattern", pattern.pattern, "is missing group", record[0])
+            sys.exit(2)
+        outputs[record[0]] = pattern
+
+    benchmark = Benchmark(outputs, command)
+    
+    # Open input and output
     reader = csv.DictReader(args.infile)
-    writer_fields = reader.fieldnames + ['time']
+    writer_fields = reader.fieldnames + [ key for key in outputs ]
     writer = csv.DictWriter(args.outfile, writer_fields)
     writer.writeheader()
-    benchmark(reader, writer, *command)
+    
+    benchmark.run(reader, writer)
     
